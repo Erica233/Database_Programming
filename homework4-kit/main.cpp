432bdfcba2a35ef95e23a08300f2a8345cb3b657
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-03-10 02:33:59 -0500

    create tables

--- homework4-kit/main.cpp
+++ homework4-kit/main.cpp
@@ -6,9 +6,61 @@
 using namespace std;
 using namespace pqxx;
 
-//void create_tables() {
-  //  sql =
-//}
+void drop_tables(connection & C) {
+    /* Create SQL statement */
+    //sql = "DROP TABLE if exists " +  + " cascade;";
+    sql = "DROP TABLE IF EXISTS PLAYER, TEAM, STATE, COLOR cascade;";
+    /* Create a transactional object. */
+    work W(C);
+
+    /* Execute SQL query */
+    W.exec( sql );
+    W.commit();
+    cout << "Drop tables successfully" << endl;
+}
+void create_tables(connection & C) {
+    string sql = "";
+    //state
+    sql += "CREATE TABLE STATE ("
+           "STATE_ID SERIAL PRIMARY KEY NOT NULL, "
+           "NAME CHAR(2));";
+    //color
+    sql += "CREATE TABLE COLOR ("
+           "COLOR_ID SERIAL PRIMARY KEY NOT NULL, "
+           "NAME VARCHAR(50));";
+    //team
+    sql += "CREATE TABLE TEAM ("
+           "TEAM_ID SERIAL PRIMARY KEY NOT NULL, "
+           "NAME VARCHAR(100),"
+           "STATE_ID INT, "
+           "COLOR_ID INT, "
+           "WINS INT, "
+           "LOSSES INT,"
+           "FOREIGN KEY (STATE_ID) REFERENCES STATE(STATE_ID) ON DELETE SET NULL ON UPDATE CASCADE,"
+           "FOREIGN KEY (COLOR_ID) REFERENCES COLOR(COLOR_ID) ON DELETE SET NULL ON UPDATE CASCADE);";
+    //player
+    sql += "CREATE TABLE PLAYER ("
+          "PLAYER_ID SERIAL PRIMARY KEY NOT NULL, "
+          "TEAM_ID INT, "
+          "UNIFORM_NUM INT, "
+          "FIRST_NAME VARCHAR(100), "
+          "LAST_NAME VARCHAR(100), "
+          "MPG INT, "
+          "PPG INT, "
+          "RPG INT, "
+          "APG INT, "
+          "SPG FLOAT, "
+          "BPG FLOAT,"
+          "FOREIGN KEY (TEAM_ID) REFERENCES TEAM(TEAM_ID) ON DELETE SET NULL ON UPDATE CASCADE);";
+
+    /* Create a transactional object. */
+    work W(C);
+
+    /* Execute SQL query */
+    W.exec( sql );
+    W.commit();
+    cout << "Table created successfully" << endl;
+}
 
 int main(int argc, char *argv[]) {
 
@@ -35,7 +87,8 @@ int main(int argc, char *argv[]) {
     //      load each table with rows from the provided source txt files
 
     //create tables (drop existed tables first)
-    //create_tables();
+    drop_tables(C);
+    create_tables(C);
     //insert
     //query
 
