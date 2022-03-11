158b92a3eb081bc47fe7acf9ce3ff6d6a4272901
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-03-10 22:58:12 -0500

    query2

--- homework4-kit/exerciser.cpp
+++ homework4-kit/exerciser.cpp
@@ -1,8 +1,16 @@
 #include "exerciser.h"
 
 void exercise(connection *C) {
+    cout << "====== test query 1 =======\n";
 
+    cout << "====== test query 2 =======\n";
     query2(C, "LightBlue");
     query2(C, "Maroon");
+
+    cout << "====== test query 3 =======\n";
+
+    cout << "====== test query 4 =======\n";
+
+    cout << "====== test query 5 =======\n";
 
 }
