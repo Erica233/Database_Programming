42212656c801ba523fbef509fc5ac988e98f4fb6
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-03-16 05:15:14 -0400

    orm - id int

--- mysite/orm/models.py
+++ mysite/orm/models.py
@@ -2,18 +2,21 @@ from django.db import models
 
 # Create your models here.
 class State(models.Model):
-    state_id = models.AutoField(primary_key=True)
+    #state_id = models.AutoField(primary_key=True)
+    state_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=10)
 
     def __str__(self):
         return self.name
 
 class Color(models.Model):
-    color_id = models.AutoField(primary_key=True)
+    #color_id = models.AutoField(primary_key=True)
+    color_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
 
 class Team(models.Model):
-    team_id = models.AutoField(primary_key=True)
+    #team_id = models.AutoField(primary_key=True)
+    team_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     state_id = models.ForeignKey('State', on_delete=models.CASCADE)
     color_id = models.ForeignKey('Color', on_delete=models.CASCADE)
@@ -21,7 +24,8 @@ class Team(models.Model):
     losses = models.IntegerField()
 
 class Player(models.Model):
-    player_id = models.AutoField(primary_key=True)
+    #player_id = models.AutoField(primary_key=True)
+    player_id = models.IntegerField(primary_key=True)
     team_id = models.ForeignKey('Team', on_delete=models.CASCADE)
     uniform_num = models.IntegerField()
     first_name = models.CharField(max_length=100)
