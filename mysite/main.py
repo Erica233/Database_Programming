2d6c1cb58c203299dc2b575eedf15777be8be84e
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-03-16 05:17:30 -0400

    orm - id int (create)

--- mysite/main.py
+++ mysite/main.py
@@ -11,25 +11,25 @@ def create_state():
     file = open("state.txt")
     for line in file:
         state_id, name = line.split(' ')
-        State.objects.create(name=name)
+        State.objects.create(state_id=state_id, name=name)
 
 def create_color():
     file = open("color.txt")
     for line in file:
         color_id, name = line.split(' ')
-        Color.objects.create(name=name)
+        Color.objects.create(color_id=color_id, name=name)
 
 def create_team():
     file = open("team.txt")
     for line in file:
         team_id, name, state_id, color_id, wins, losses = line.split(' ')
-        Team.objects.create(name=name, state_id=state_id, color_id=color_id, wins=wins, losses=losses)
+        Team.objects.create(team_id=team_id, name=name, state_id=state_id, color_id=color_id, wins=wins, losses=losses)
 
 def create_player():
     file = open("player.txt")
     for line in file:
         player_id, team_id, uniform_num, first_name, last_name, mpg, ppg, rpg, apg, spg, bpg = line.split(' ')
-        Player.objects.create(team_id=team_id, uniform_num=uniform_num, first_name=first_name, last_name=last_name, mpg=mpg, ppg=ppg, rpg=rpg, apg=apg, spg=spg, bpg=bpg)
+        Player.objects.create(player_id=player_id, team_id=team_id, uniform_num=uniform_num, first_name=first_name, last_name=last_name, mpg=mpg, ppg=ppg, rpg=rpg, apg=apg, spg=spg, bpg=bpg)
 
 def drop_tables():
     Player.objects.all().delete()
