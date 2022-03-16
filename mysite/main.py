8e5f2da7670b6f7ad9c78da23e0236dd257f8f86
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-03-16 05:32:01 -0400

    orm - fk insert

--- mysite/main.py
+++ mysite/main.py
@@ -12,24 +12,28 @@ def create_state():
     for line in file:
         state_id, name = line.split(' ')
         State.objects.create(state_id=state_id, name=name)
+    file.close()
 
 def create_color():
     file = open("color.txt")
     for line in file:
         color_id, name = line.split(' ')
         Color.objects.create(color_id=color_id, name=name)
+    file.close()
 
 def create_team():
     file = open("team.txt")
     for line in file:
         team_id, name, state_id, color_id, wins, losses = line.split(' ')
-        Team.objects.create(team_id=team_id, name=name, state_id=state_id, color_id=color_id, wins=wins, losses=losses)
+        Team.objects.create(team_id=team_id, name=name, state_state_id=state_id, color_id=color_id, wins=wins, losses=losses)
+    file.close()
 
 def create_player():
     file = open("player.txt")
     for line in file:
         player_id, team_id, uniform_num, first_name, last_name, mpg, ppg, rpg, apg, spg, bpg = line.split(' ')
         Player.objects.create(player_id=player_id, team_id=team_id, uniform_num=uniform_num, first_name=first_name, last_name=last_name, mpg=mpg, ppg=ppg, rpg=rpg, apg=apg, spg=spg, bpg=bpg)
+    file.close()
 
 def drop_tables():
     Player.objects.all().delete()
