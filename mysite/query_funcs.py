eae881c041557909fea46cfe0de2d05ed3d550a9
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-03-16 23:14:54 -0400

    orm - query2

--- mysite/query_funcs.py
+++ mysite/query_funcs.py
@@ -59,7 +59,7 @@ def query1(use_mpg, min_mpg, max_mpg,
 
 def query2(team_color):
     print(team_color)
-    output = Team.objects.filter(color_id_name=team_color)
+    output = Team.objects.filter(color_id__name=team_color)
     print(output)
     print("NAME")
     for team in output:
